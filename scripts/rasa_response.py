ea39e04f09d4cd51383124bf1bab4212bcf45a0c
changchuntzu0618 <chun-tzu.chang@epfl.ch>
2023-11-20 10:06:41 +0100

    change to use end of speech eos

--- scripts/rasa_response.py
+++ scripts/rasa_response.py
@@ -38,6 +38,7 @@
 
 import rospy
 from std_msgs.msg import String
+from end_of_speech.msg import EndOfSpeech
 import requests
 
 sender = "user"
@@ -47,20 +48,23 @@ rasa_endpoint = "http://localhost:5005/webhooks/rest/webhook"
 
 class RASA:
     def __init__(self):
-        self.sub=rospy.Subscriber('/human_dialogue', String, self.rasa_response)
+        # self.sub=rospy.Subscriber('/human_dialogue', String, self.rasa_response)
+        self.sub=rospy.Subscriber('/end_of_speech/eos', EndOfSpeech, self.rasa_response)
         self.pub= rospy.Publisher('~rasa_response', String, queue_size=1)
 
-    def rasa_response(self, data):
-        text=data.data
-        rospy.loginfo('Heard User said: "%s" ' % text)
+    def rasa_response(self, eos_msg):
+        self.text=eos_msg.final_utterance
+        self.confidence=eos_msg.confidence
+        self.header=eos_msg.header
+        rospy.loginfo('Heard User said: "%s", with confidence "%f" ' % (self.text, self.confidence))
 
-        results = requests.post(
-            rasa_endpoint, json={"sender": sender, "message": text}
+        self.results = requests.post(
+            rasa_endpoint, json={"sender": sender, "message": self.text}
         ).json()
         
-        for result in results:
+        for result in self.results:
             result=result['text']
-            rospy.loginfo('Rasa response: "%s" ' % result)
+            rospy.loginfo('Rasa response: "%s" ' % (result))
             self.pub.publish(result)
             
 
