15d7cdbfb31e6120c25fbc5a323f7e062dce1f3f
changchuntzu0618 <chun-tzu.chang@epfl.ch>
2023-12-03 19:41:46 +0100

    add emotion in rasa input

--- scripts/rasa_response.py
+++ scripts/rasa_response.py
@@ -16,23 +16,36 @@ class RASA:
         self.sub=rospy.Subscriber('/end_of_speech/eos', EndOfSpeech, self.rasa_response)
         
         self.pub= rospy.Publisher('~rasa_response', String, queue_size=1)
+        self.get_emotion=False
 
     def rasa_response(self, eos_msg):
         self.text=eos_msg.final_utterance
         self.confidence=eos_msg.confidence
         self.header=eos_msg.header
         rospy.loginfo('Heard User said: "%s", with confidence "%f" ' % (self.text, self.confidence))
-        self.emotion=rospy.wait_for_message('/fer/emotion', String, timeout=10)
 
         self.results = requests.post(
             rasa_endpoint, json={"sender": sender, "message": self.text}
         ).json()
         
-        for result in self.results:
-            result=result['text']
-            rospy.loginfo('Rasa response: "%s" ' % (result))
-            self.pub.publish(result)
-            
+        
+        if self.results:
+            self.emotion=rospy.wait_for_message('/fer/emotion', String, timeout=10)
+            content='I am feeling '+str(self.emotion.data)
+            self.results_emotion = requests.post(
+                rasa_endpoint, json={"sender": sender, "message": content}
+            ).json()
+        
+            for result in self.results_emotion:
+                result=result['text']
+                rospy.loginfo('Rasa response: "%s" ' % (result))
+                self.pub.publish(result)
+        else:
+            for result in self.results:
+                result=result['text']
+                rospy.loginfo('Rasa response: "%s" ' % (result))
+                self.pub.publish(result)
+
 
 
 if __name__ == '__main__':
