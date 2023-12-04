a3a4198734b8e5d4a290354544049c2b3cee50bf
changchuntzu0618 <chun-tzu.chang@epfl.ch>
2023-12-04 12:21:48 +0100

    rasa response with emotion finish

--- scripts/rasa_response.py
+++ scripts/rasa_response.py
@@ -9,6 +9,8 @@ sender = "user"
 
 rasa_endpoint = "http://localhost:5005/webhooks/rest/webhook"
 
+utter_think='Let me think about it'
+detect_emotion='detect emotion'
 
 class RASA:
     def __init__(self):
@@ -24,28 +26,41 @@ class RASA:
         self.header=eos_msg.header
         rospy.loginfo('Heard User said: "%s", with confidence "%f" ' % (self.text, self.confidence))
 
-        self.results = requests.post(
-            rasa_endpoint, json={"sender": sender, "message": self.text}
-        ).json()
+        self.send_to_rasa(self.text)
         
-        
-        if self.results:
-            self.emotion=rospy.wait_for_message('/fer/emotion', String, timeout=10)
-            content='I am feeling '+str(self.emotion.data)
-            self.results_emotion = requests.post(
-                rasa_endpoint, json={"sender": sender, "message": content}
-            ).json()
-        
-            for result in self.results_emotion:
-                result=result['text']
-                rospy.loginfo('Rasa response: "%s" ' % (result))
-                self.pub.publish(result)
-        else:
-            for result in self.results:
-                result=result['text']
-                rospy.loginfo('Rasa response: "%s" ' % (result))
-                self.pub.publish(result)
+       
 
+    def send_to_rasa(self, text):
+        results = requests.post(
+            rasa_endpoint, json={"sender": sender, "message": text}
+        ).json()
+        print('first:',results)
+        for result in results:
+            result=result['text']
+            if result==detect_emotion:
+                self.emotion_to_rasa()
+            elif result==utter_think:
+                self.publish_to_result(result)
+                self.utter_think_func()
+            else:
+                self.publish_to_result(result)
+        
+    
+    def publish_to_result(self, result):
+        rospy.loginfo('Rasa response: "%s" ' % (result))
+        self.pub.publish(result)
+
+    def emotion_to_rasa(self):
+        # self.emotion=rospy.wait_for_message('/fer/emotion', String, timeout=10)
+        # content='I am feeling '+str(self.emotion.data)
+
+        # for testing
+        self.emotion='sad'
+        content='I am feeling '+str(self.emotion)
+        self.send_to_rasa(content)
+    
+    def utter_think_func(self):
+        self.send_to_rasa('start chatgpt') 
 
 
 if __name__ == '__main__':
