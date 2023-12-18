79fa75446624ce455f94a93fb19526e56ec80cfb
changchuntzu0618 <chun-tzu.chang@epfl.ch>
2023-12-18 16:18:27 +0100

    udpate

--- scripts/rasa_response.py
+++ scripts/rasa_response.py
@@ -33,13 +33,20 @@ class RASA:
             return
         self.confidence=eos_msg.confidence
         self.header=eos_msg.header
-        self.talktime_buffer=rospy.wait_for_message('/pepper_say/talk_time', PepperTalkTime, timeout=10)
-        print('self.header.timestamp.to_sec():',self.header.timestamp.to_sec())
-        print('self.talktime_buffer.start_stamp.to_sec():',self.talktime_buffer.start_stamp.to_sec())
-        print('self.talktime_buffer.finish_stamp.to_sec():',self.talktime_buffer.finish_stamp.to_sec())
-        if self.header.timestamp.to_nsec()<=self.talktime_buffer.finish_stamp.to_nsec() and self.header.timestamp.to_nsec()>=self.talktime_buffer.start_stamp.to_nsec():
-            print('It is pepper talking')
-            return
+        if self.talktime_buffer is not None:
+            print('self.header.stamp.to_sec():',self.header.stamp.to_sec())
+            print('self.talktime_buffer.start_stamp.to_sec():',self.talktime_buffer.start_stamp.to_sec())
+            print('self.talktime_buffer.finish_stamp.to_sec():',self.talktime_buffer.finish_stamp.to_sec())
+            if self.header.stamp.to_nsec()<=self.talktime_buffer.start_stamp.to_nsec():
+                print('It is pepper talking')
+                return
+            if self.header.stamp.to_nsec()-self.talktime_buffer.finish_stamp.to_nsec()<=4*10**9:
+                print('It is pepper talking')
+                return
+            # if self.header.stamp.to_nsec()<=self.talktime_buffer.finish_stamp.to_nsec() and 
+            #     print('It is pepper talking')
+            #     return
+        
         
         rospy.loginfo('Heard User said: "%s", with confidence "%f" ' % (self.text, self.confidence))
 
@@ -69,13 +76,14 @@ class RASA:
         rospy.loginfo('Rasa response: "%s" ' % (result))
         self.pub.publish(result)
         self.talktime_buffer=rospy.wait_for_message('/pepper_say/talk_time', PepperTalkTime, timeout=10)
-        self.talktime_buffer.start_stamp=rospy.Time.from_sec((self.talktime_buffer.start_stamp.to_sec()+self.talktime_buffer.finish_stamp.to_sec())/2)
-        self.talktime_buffer.finish_stamp=rospy.Time.from_sec(self.talktime_buffer.finish_stamp.to_sec()+2)
+        self.talktime_buffer_send=copy.deepcopy(self.talktime_buffer)
+        self.talktime_buffer_send.start_stamp=rospy.Time.from_sec((self.talktime_buffer.start_stamp.to_sec()+self.talktime_buffer.finish_stamp.to_sec())/2)
+        self.talktime_buffer_send.finish_stamp=rospy.Time.from_sec(self.talktime_buffer.finish_stamp.to_sec()+2)
         # print('self.talktime_buffer:',self.talktime_buffer)
         
     def get_emotion_pepper_talk(self):
         time.sleep(3) #wait for 3 second
-        emotion=self.srv_emotion('pepper',self.talktime_buffer.start_stamp, self.talktime_buffer.finish_stamp).emotion
+        emotion=self.srv_emotion('pepper',self.talktime_buffer_send.start_stamp, self.talktime_buffer_send.finish_stamp).emotion
         rospy.loginfo('Get emotion (pepper talk): '+str(emotion))
         self.emotion_to_rasa(emotion)
     
