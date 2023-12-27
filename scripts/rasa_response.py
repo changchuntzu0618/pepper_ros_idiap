7b674ab36f95992c96f3fe7fdcdc2473c5265e4f
changchuntzu0618 <chun-tzu.chang@epfl.ch>
2023-12-27 18:32:42 +0100

    add comment to rasa response

--- scripts/rasa_response.py
+++ scripts/rasa_response.py
@@ -28,12 +28,20 @@ class RASA:
         self.talktime_buffer=None
 
     def rasa_response(self, eos_msg):
+        """
+        Process the end-of-speech message received from speech recognition node "/end_of_speech/eos", and 
+        and forward the user's utterance to a Rasa system for further processing.
+
+        Args:
+            eos_msg (EndOfSpeech): End-of-speech message containing the final user utterance and confidence.
+        """
         self.text=eos_msg.final_utterance
         if self.text=='' or self.text==' ':
             return
         self.confidence=eos_msg.confidence
         self.header=eos_msg.header
         if self.talktime_buffer is not None:
+            # if the message is less than 4 seconds of previous messgae, then ignore it and consider it as pepper is talking
             if self.header.stamp.to_nsec()<=self.talktime_buffer.start_stamp.to_nsec() or self.header.stamp.to_nsec()-self.talktime_buffer.finish_stamp.to_nsec()<=4*10**9:
                 print('It is pepper talking')
                 return
@@ -44,8 +52,13 @@ class RASA:
         self.send_to_rasa(self.text)
         
        
-
     def send_to_rasa(self, text):
+        """
+        Send the user's utterance to a Rasa system and get response.
+
+        Args:
+            text (str): User's utterance.
+        """
         results = requests.post(
             rasa_endpoint, json={"sender": sender, "message": text}
         ).json()
@@ -63,6 +76,13 @@ class RASA:
             
     
     def publish_to_result(self, result):
+        """
+        Publish the response from Rasa to the topic "/rasa/rasa_response" which send the text to
+        Pepper, and Pepper will say it.
+
+        Args:
+            result (str): Response from Rasa.
+        """
         rospy.loginfo('Rasa response: "%s" ' % (result))
         self.pub.publish(result)
         self.talktime_buffer=rospy.wait_for_message('/pepper_say/talk_time', PepperTalkTime, timeout=10)
@@ -71,17 +91,29 @@ class RASA:
         self.talktime_buffer_send.finish_stamp=rospy.Time.from_sec(self.talktime_buffer.finish_stamp.to_sec()+2)
         
     def get_emotion_pepper_talk(self):
+        """
+        Get the emotion of user when Pepper is talking.
+        """
         time.sleep(3) #wait for 3 second
         emotion=self.srv_emotion('pepper',self.talktime_buffer_send.start_stamp, self.talktime_buffer_send.finish_stamp).emotion
         rospy.loginfo('Get emotion (pepper talk): '+str(emotion))
         self.emotion_to_rasa(emotion)
     
     def get_emotion_user_talk(self):
+        """
+        Get the emotion of user when user is talking.
+        """
         emotion = self.srv_emotion('user',None,None).emotion
         rospy.loginfo('Get emotion (user talk): '+str(emotion))
         self.emotion_to_rasa(emotion)
 
     def emotion_to_rasa(self, emotion):
+        """
+        Send the emotion to Rasa in the form of a sentence.
+
+        Args:
+            emotion (str): Emotion of user.
+        """
         if emotion == 'no_person_detected' or emotion == 'no_face':
             emotion = 'neutral'
 
